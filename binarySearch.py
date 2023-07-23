fb00642793707af2ccb293bd6b46adf15fe746ea
hurdaw <tn133931@gmail.com>
2023-07-23 20:28:11 +0700

    figured out

--- binarySearch.py
+++ binarySearch.py
@@ -12,16 +12,44 @@ def binary_search(list, key):
         if list[middle] == key:
             return middle
         if list[middle] > key:
+            print("Checking the left side")
             right = middle - 1
         if list[middle] < key:
+            print("Checking the right side")
             left = middle + 1
 
     # Key not found, return -1
     return -1
 
 
-print(binary_search([10, 2, 9, 6, 7, 1, 5, 3, 4, 8], 1))  # 2
-print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5))  # 4
-print(binary_search([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], 7))  # 6
-print(binary_search([1, 3, 5, 7, 9, 10, 2, 4, 6, 8], 10))  # 9
-print(binary_search([5, 1, 8, 2, 4, 10, 7, 6, 3, 9], 11))  # -1
+print(binary_search([10, 2, 9, 6, 7, 1, 5, 3, 4, 8], 1))
+# Should print 2 debug lines and the return value:
+# Checking the left side
+# Checking the left side
+# 0
+
+print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5))
+# Should print no debug lines, as it's located immediately:
+# 4
+
+print(binary_search([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], 7))
+# Should print 3 debug lines and the return value:
+# Checking the right side
+# Checking the left side
+# Checking the right side
+# 6
+
+print(binary_search([1, 3, 5, 7, 9, 10, 2, 4, 6, 8], 10))
+# Should print 3 debug lines and the return value:
+# Checking the right side
+# Checking the right side
+# Checking the right side
+# 9
+
+print(binary_search([5, 1, 8, 2, 4, 10, 7, 6, 3, 9], 11))
+# Should print 4 debug lines and the "not found" value of -1:
+# Checking the right side
+# Checking the right side
+# Checking the right side
+# Checking the right side
+# -1
